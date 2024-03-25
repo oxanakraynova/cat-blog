d95dcb0f19ec3d5c075424e229afc7b44db7f5ec
Oxana Kraynova <oxana.kraynova@dtforce.com>
2024-03-25 20:06:08 +0100

    - add interceptors to a custom config for token

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,13 +1,24 @@
 import axios from "axios";
 
 const apiKey = "c98db5eb-b5f8-4ebc-8e8d-8281f7e6ec22";
-const bearerToken = "Bearer 17ffeeee-82c9-4aed-a6ca-e4155c28ae6d";
 
 export const apiClient = axios.create({
   baseURL: "https://fullstack.exercise.applifting.cz",
   headers: {
     "Content-Type": "application/json",
     "X-API-KEY": apiKey,
-    Authorization: bearerToken,
   },
 });
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
