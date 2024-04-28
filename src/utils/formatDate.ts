ba00e2d8ebbeac89c750c9ed1566c8fbc0598995
Oxana Kraynova <oxana.kraynova@dtforce.com>
2024-04-28 16:35:18 +0200

    - use moment.js for date formatting

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,15 +1,11 @@
+import moment from "moment";
+
 export function formatDate(dateStr: string) {
-  const date = new Date(dateStr);
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  const year = String(date.getFullYear()).slice(-2);
-  return `${month}/${day}/${year}`;
+  const formattedDate = moment(dateStr).format("MM/DD/YY");
+  return formattedDate;
 }
 
 export function customFormatDate(dateStr: string) {
-  const date = new Date(dateStr);
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  const year = String(date.getFullYear());
-  return `${day}.${month}.${year}`;
+  const formattedDate = moment(dateStr).format("DD.MM.YY");
+  return formattedDate;
 }
